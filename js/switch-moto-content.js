9c9f39e638681defb6bc0099e71d1797be3ccb67
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-09-17 17:34:58 +0500

    Правки переключения пунктов меню

--- js/switch-moto-content.js
+++ js/switch-moto-content.js
@@ -1,9 +1,20 @@
 (function() {
-  const models_block = document.querySelector(`.models__list`);
+  const nav_menu = document.querySelector(`.site-menu`);
+  const models_menu = document.querySelector(`.models__list`);
   let curr_model = document.querySelector(`.model--current`);
+  let curr_nav_item = document.querySelector(`.site-menu__item-current`);
+  
+  const CurrentEl = {
+    SITE_MENU: `site-menu__item-current`,
+    MOTO_MENU: `model--current`
+  };
+
+  const navMenuClickHandler = (evt) => {
+    
+  };
 
   // Обработчик кликов по мото-меню
-  const modelsBlockClickHandler = (evt) => {
+  const modelsMenuClickHandler = (evt) => {
     const clickedEl = evt.target.closest(`li`);
     
     curr_model.classList.remove(`model--current`);
@@ -11,6 +22,6 @@
     curr_model = clickedEl;
   };
   
-  // Вешаем обработчик на блок мото-меню
-  models_block.addEventListener(`click`, modelsBlockClickHandler);
+  nav_menu.addEventListener(`click`, navMenuClickHandler);
+  models_menu.addEventListener(`click`, modelsMenuClickHandler);
 })();
